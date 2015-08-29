812c3fafd9a9396ec25f5199bad683ec96be80b0
Melissa Porto <melroman26@gmail.com>
2015-08-29 04:01:06 -0700

    Messed with gradients

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,19 +1,23 @@
 var gradient = [
-    'rgba(0, 255, 255, 0)',
-    'rgba(0, 255, 255, 1)',
-    'rgba(0, 191, 255, 1)',
-    'rgba(0, 127, 255, 1)',
-    'rgba(0, 63, 255, 1)',
-    'rgba(0, 0, 255, 1)',
-    'rgba(0, 0, 223, 1)',
-    'rgba(0, 0, 191, 1)',
-    'rgba(0, 0, 159, 1)',
-    'rgba(0, 0, 127, 1)',
-    'rgba(63, 0, 91, 1)',
-    'rgba(127, 0, 63, 1)',
+    'rgba(0, 255, 255, 0)', //turquoise
+    'rgba(0, 255, 255, 1)', //turquoise
+    'rgba(0, 191, 255, 1)', //light blue
+    'rgba(0, 127, 255, 1)', //bright light blue
+    'rgba(0, 63, 255, 1)', //bright blue
+    'rgba(0, 0, 255, 1)', //blue
+    'rgba(0, 0, 223, 1)', //blue
+    'rgba(0, 0, 191, 1)', //light royal blue
+    'rgba(0, 0, 159, 1)', //royal blue
+    'rgba(0, 0, 127, 1)', //dark blue
+    'rgba(63, 0, 91, 1)', //dark purple
+    'rgba(127, 0, 63, 1)', //reddish purple
     'rgba(191, 0, 31, 1)',
     'rgba(255, 0, 0, 1)'
   ];
+
+
+
+
 
 function initMap() {
   var directionsService = new google.maps.DirectionsService();
